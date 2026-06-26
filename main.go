8cdcd7dd655dfb96c3255253cf9a59324005203f
mbirbeck <mbirbeck@Mac.lan>
2026-06-26 11:59:35 +0100

    matt tag test

--- main.go
+++ main.go
@@ -72,7 +72,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		name = "Guest"
 	}
 	log.Printf("Received request for %s\n", name)
-	fmt.Fprintf(w, "Hello, %s! v0.0.40\n", name)
+	fmt.Fprintf(w, "Hello, %s! mattn", name)
 }
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
